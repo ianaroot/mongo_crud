39a9d1e195f5794ee5f18e50bee45299a9343ccd
ian root <ianaroot@gmail.com>
2014-01-04 19:57:04 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoidCrud::Application.config.secret_token = '464250ecad0a200de5a2d9c587122bf2a3d24dd56cfaaa156a3631330f4199e41760e027ab7b5dedd9a45883875f75f94ad53afbe24c2f37659e3433f89f73c6'
