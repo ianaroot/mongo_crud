5d29ff8a6efdb499ec174f98b1ab8b0b302de335
ian root <ianaroot@gmail.com>
2014-01-09 10:39:39 -0800

    add bcrypt, implement has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User
   include Mongoid::Document
+  has_secure_password
   field :first_name, type: String
   field :last_name, type: String
   field :email, type: String
   validates_uniqueness_of :email
+  field :password_digest, type: String
 end
