ccd0b645498d8ed20664bd5e8481971bf5263afb
ian root <ianaroot@gmail.com>
2014-01-09 12:42:16 -0800

    user with proper email can sign in, WIP: sad path broken

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,14 @@ class SessionsController < ApplicationController
     
   end
 
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password]) 
+      sign_in(user)
+      redirect_to user_path(user)
+    else
+      redirect_to new_session_path
+    end
+  end
+
 end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,6 @@
+module SessionsHelper
+
+  def sign_in(user)
+    cookies.permanent[:user_id] = user.id
+  end
+end
