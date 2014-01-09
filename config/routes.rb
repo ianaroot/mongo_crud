95324215828a2ba380941fb7085775334be2da8d
ian root <ianaroot@gmail.com>
2014-01-09 10:53:59 -0800

    add sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ MongoidCrud::Application.routes.draw do
 
   root to: "users#new"
   resources :users, except: [:index]
+  resources :sessions, only: [:new, :create, :destroy]
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
