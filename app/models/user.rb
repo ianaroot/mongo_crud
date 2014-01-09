57085319c4b29e699ce2e2ae9847c2c14d74be67
ian root <ianaroot@gmail.com>
2014-01-09 10:52:45 -0800

    add ActiveModel::SecurePassword to fix has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User
   include Mongoid::Document
-  has_secure_password
+  include ActiveModel::SecurePassword
   field :first_name, type: String
   field :last_name, type: String
   field :email, type: String
   validates_uniqueness_of :email
   field :password_digest, type: String
+  has_secure_password
 end
