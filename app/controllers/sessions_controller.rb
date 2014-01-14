5fb4e4753a1f0c7d5c5bb203c063db8ce119c12a
ian root <ianaroot@gmail.com>
2014-01-14 14:19:20 -0800

    implement rescue to stop DocumentNotFound Error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
+    begin
+      user = User.find_by(email: params[:email])
+    rescue
+      user = nil
+    end    
     if user && user.authenticate(params[:password]) 
       sign_in(user)
       redirect_to user_path(user)
